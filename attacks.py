43f77f2f02f854598dd198ea8d659690c6257737
Matteo Bitussi <matteobitussi@gmail.com>
2024-10-25 17:45:04 +0200

    feat: added attack queue

--- attacks.py
+++ attacks.py
@@ -1,4 +1,5 @@
-from io import BytesIO
+import os
+from enum import Enum
 
 import numpy as np
 from PIL import Image
@@ -22,6 +23,15 @@ def show(*args: np.ndarray):
         plt.imshow(args[i], cmap='gray')
 
     plt.show()
+
+
+class Attack(Enum):
+    BLUR = 0
+    AWGN = 1
+    SHARPEN = 2
+    MEDIAN = 3
+    RESIZING = 4
+    JPEG = 5
 
 
 def attack_blur(img: np.ndarray, sigma) -> np.ndarray:
@@ -52,7 +62,7 @@ def attack_median(img: np.ndarray, kernel_size: int) -> np.ndarray:
     return attacked
 
 
-def resizing(img: np.ndarray, scale: float) -> np.ndarray:
+def attack_resizing(img: np.ndarray, scale: float) -> np.ndarray:
     x, y = img.shape
     attacked = rescale(img, scale)
     attacked = rescale(attacked, 1 / scale)
@@ -60,15 +70,70 @@ def resizing(img: np.ndarray, scale: float) -> np.ndarray:
     return attacked
 
 
-def jpeg_compression(img: np.ndarray, QF: int):
+def attack_jpeg(img: np.ndarray, QF: int):
     img = Image.fromarray(img)
-    output = BytesIO()
-    img.save(output, format='JPEG', quality=QF)
-    attacked = output.getvalue()
+    img.save('tmp.jpg', "JPEG", quality=QF)
+    attacked = Image.open('tmp.jpg')
     attacked = np.asarray(attacked, dtype=np.uint8)
-    # img.save('tmp.jpg',"JPEG", quality=QF)
-    # attacked = Image.open('tmp.jpg')
-    # attacked = np.asarray(attacked,dtype=np.uint8)
-    # os.remove('tmp.jpg')
+    os.remove('tmp.jpg')
+    return attacked
+
+
+a_map = {
+    Attack.BLUR: attack_blur,
+    Attack.MEDIAN: attack_median,
+    Attack.JPEG: attack_jpeg,
+    Attack.AWGN: attack_AWGN,
+    Attack.SHARPEN: attack_sharpen,
+    Attack.RESIZING: attack_resizing
+}
+
+demo_attacks = [
+    {
+        "attack": Attack.BLUR,
+        "params": {
+            "sigma": [1, 1]
+        }
+    },
+    {
+        "attack": Attack.JPEG,
+        "params": {
+            "QF": 20
+        }
+    }
+]
+
+
+def apply_attack_queue(im: np.ndarray, l: list) -> np.ndarray:
+    """
+    Apply the given list of attacks to the given image and return the attacked image.
+    demo_attacks = [
+    {
+        "attack": Attack.BLUR,
+        "params": {
+            "sigma": [1,1]
+        }
+    }
+    ]
+    """
+    attacked: np.ndarray = im
+
+    for a in l:
+        a_type: Attack = a["attack"]
+        a_params: dict[any] = a["params"]
+        if a_type == Attack.SHARPEN:
+            attacked = attack_sharpen(attacked, a_params["sigma"], a_params["alpha"])
+        elif a_type == Attack.BLUR:
+            attacked = attack_blur(attacked, a_params["sigma"])
+        elif a_type == Attack.AWGN:
+            attacked = attack_AWGN(attacked, a_params["std"], a_params["seed"], a_params["mean"])
+        elif a_type == Attack.JPEG:
+            attacked = attack_jpeg(attacked, a_params["QF"])
+        elif a_type == Attack.MEDIAN:
+            attacked = attack_median(attacked, a_params["kernel_size"])
+        elif a_type == Attack.RESIZING:
+            attacked = attack_resizing(attacked, a_params["scale"])
+        else:
+            raise KeyError(f"invalid attack in dict: {a_type}")
 
     return attacked

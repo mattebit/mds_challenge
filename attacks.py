5f32aa3d246b669bcac8efe15e3b17e283282796
Matteo Bitussi <matteobitussi@gmail.com>
2024-10-25 09:15:35 +0200

    feat: added all attacks to file

--- attacks.py
+++ attacks.py
@@ -1,5 +1,11 @@
+from io import BytesIO
+
 import numpy as np
+from PIL import Image
 from matplotlib import pyplot as plt
+from scipy.ndimage import gaussian_filter
+from scipy.signal import medfilt
+from skimage.transform import rescale
 
 
 def show(*args: np.ndarray):
@@ -18,10 +24,51 @@ def show(*args: np.ndarray):
     plt.show()
 
 
-def attack_blur(img: np.ndarray, sigma):
+def attack_blur(img: np.ndarray, sigma) -> np.ndarray:
     """
     attack_blur(im, [2,2])
     """
     from scipy.ndimage.filters import gaussian_filter
     attacked = gaussian_filter(img, sigma)
+    return attacked
+
+
+def attack_AWGN(img: np.ndarray, std: int, seed: float, mean: float = 0.0) -> np.ndarray:
+    mean = mean  # some constant
+    np.random.seed(seed)
+    attacked = img + np.random.normal(mean, std, img.shape)
+    attacked = np.clip(attacked, 0, 255)
+    return attacked
+
+
+def attack_sharpen(img: np.ndarray, sigma: int, alpha: int) -> np.ndarray:
+    filter_blurred_f = gaussian_filter(img, sigma)
+    attacked = img + alpha * (img - filter_blurred_f)
+    return attacked
+
+
+def attack_median(img: np.ndarray, kernel_size: int) -> np.ndarray:
+    attacked = medfilt(img, kernel_size)
+    return attacked
+
+
+def resizing(img: np.ndarray, scale: float) -> np.ndarray:
+    x, y = img.shape
+    attacked = rescale(img, scale)
+    attacked = rescale(attacked, 1 / scale)
+    attacked = attacked[:x, :y]
+    return attacked
+
+
+def jpeg_compression(img: np.ndarray, QF: int):
+    img = Image.fromarray(img)
+    output = BytesIO()
+    img.save(output, format='JPEG', quality=QF)
+    attacked = output.getvalue()
+    attacked = np.asarray(attacked, dtype=np.uint8)
+    # img.save('tmp.jpg',"JPEG", quality=QF)
+    # attacked = Image.open('tmp.jpg')
+    # attacked = np.asarray(attacked,dtype=np.uint8)
+    # os.remove('tmp.jpg')
+
     return attacked

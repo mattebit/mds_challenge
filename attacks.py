6e83d16d387dbf7f2bab340b88a20d536bd9b6d0
Matteo Bitussi <matteobitussi@gmail.com>
2024-10-26 17:52:57 +0200

    added joint attacks and some fixes

--- attacks.py
+++ attacks.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import uuid
 from concurrent.futures import ProcessPoolExecutor
@@ -77,13 +78,22 @@ def attack_AWGN(img: np.ndarray, std: int, seed: float, mean: float = 0.0) -> np
     return attacked
 
 
-def attack_sharpen(img: np.ndarray, sigma: int, alpha: int) -> np.ndarray:
+def attack_sharpen(img: np.ndarray, sigma: float, alpha: float) -> np.ndarray:
     filter_blurred_f = gaussian_filter(img, sigma)
     attacked = img + alpha * (img - filter_blurred_f)
     return attacked
 
 
 def attack_median(img: np.ndarray, kernel_size: int) -> np.ndarray:
+    """
+    best value kernel_size = 3, higher values cause lower wpsnr linearly
+    Args:
+        img:
+        kernel_size:
+
+    Returns:
+
+    """
     attacked = medfilt(img, kernel_size)
     return attacked
 
@@ -93,17 +103,16 @@ def attack_resizing(img: np.ndarray, scale: float) -> np.ndarray:
     attacked = rescale(img, scale)
     attacked = rescale(attacked, 1 / scale)
     attacked = attacked[:x, :y]
-    if attacked.shape[0] != x or attacked.shape[1] != y:
-        attacked = cv2.resize(attacked, (x, y))
     return attacked
 
 
 def attack_jpeg(img: np.ndarray, QF: int):
     img = Image.fromarray(img)
-    img.save('tmp.jpg', "JPEG", quality=QF)
-    attacked = Image.open('tmp.jpg')
+    tmp_path = os.path.join(CACHE_DIR, str(uuid.uuid4()) + ".jpg")
+    img.save(tmp_path, "JPEG", quality=QF)
+    attacked = Image.open(tmp_path)
     attacked = np.asarray(attacked, dtype=np.uint8)
-    os.remove('tmp.jpg')
+    os.remove(tmp_path)
     return attacked
 
 
@@ -169,69 +178,106 @@ def apply_attack_queue(im: str,
             attacking = attack_resizing(attacking, a_params["scale"])
         else:
             raise KeyError(f"invalid attack in dict: {a_type}")
-    print("Attack applied: ", l)
+
     if detection_function is not None:
         detection_attacked_path = os.path.join(CACHE_PATH, f"attacked_{uuid.uuid4()}.bmp")
         cv2.imwrite(detection_attacked_path, attacking)
         contains_w, wpsnr = detection_function(ORIGINAL_IMG_PATH, im, detection_attacked_path)
-    return attacking, contains_w, wpsnr
+        if contains_w == 0:  # TODO remove
+            print(f"Contains w?: {contains_w}, WPSNR: {wpsnr}. Attack: {l}")
+    else:
+        print("Attack applied: ", l)
+
+    return attacking, contains_w, wpsnr, l
+
+
+def prepare_attacks():
+    blur = lambda x, y: {"attack": Attack.BLUR, "params": {"sigma": [x, y]}}
+    median = lambda x: {"attack": Attack.MEDIAN, "params": {"kernel_size": x}}
+    jpeg = lambda x: {"attack": Attack.JPEG, "params": {"QF": x}}
+    sharpen = lambda x, y: {"attack": Attack.SHARPEN, "params": {"sigma": x, "alpha": y}}
+    awgn = lambda x, y, z: {"attack": Attack.AWGN, "params": {"std": x, "seed": y, "mean": z}}
+    resizing = lambda x: {"attack": Attack.RESIZING, "params": {"scale": x}}
+
+    res = []
+
+    if False:
+        for i in range(10, 150, 10):
+            res.append([blur(i / 100, i / 100)])
+
+    if False:
+        for i in range(1, 6, 2):
+            res.append([median(i)])
+
+    if False:
+        for i in range(1, 40, 1):
+            res.append([jpeg(i)])
+
+    # Most time-consuming attacks:
+    if False:
+        for i in range(50, 100, 10):
+            for j in range(1, 6, 1):
+                res.append([sharpen(i / 100, j)])  # do not convert j to float
+
+    if False:
+        for i in range(13, 16):
+            for j in range(30, 120, 10):
+                res.append([awgn(i, j, 0.0)])
+
+    if False:
+        for i in range(1, 100):
+            res.append([resizing(i / 100)])
+
+    return res
+
+
+def prepare_joint_attacks():
+    blur = lambda x, y: {"attack": Attack.BLUR, "params": {"sigma": [x, y]}}
+    median = lambda x: {"attack": Attack.MEDIAN, "params": {"kernel_size": x}}
+    jpeg = lambda x: {"attack": Attack.JPEG, "params": {"QF": x}}
+    sharpen = lambda x, y: {"attack": Attack.SHARPEN, "params": {"sigma": x, "alpha": y}}
+    awgn = lambda x, y, z: {"attack": Attack.AWGN, "params": {"std": x, "seed": y, "mean": z}}
+
+    res = []
+
+    for i in range(1, 50, 5):
+        for j in range(95, 101):
+            act = []
+            # act.append(sharpen(i / 100, 1))
+            # act.append(awgn(i, 1, 0.0))
+            if False:
+                act.append(blur((i + 10) / 100, (i + 10) / 100))
+                act.append(jpeg(j))
+            elif True:
+                act.append(jpeg(j))
+                act.append(blur((i) / 100, (i) / 100))
+            res.append(act)
+
+    return res
 
 
 if __name__ == "__main__":
-    attacks = [[
-        {
-            "attack": Attack.BLUR,
-            "params": {
-                "sigma": [1, 1]
-            }
-        },
-    ], [
-        {
-            "attack": Attack.JPEG,
-            "params": {
-                "QF": 20
-            }
-        }
-    ], [
-        {
-            "attack": Attack.AWGN,
-            "params": {
-                "std": 10,
-                "seed": 0,
-                "mean": 0
-            }
-        }
-    ], [
-        {
-            "attack": Attack.MEDIAN,
-            "params": {
-                "kernel_size": 3
-            }
-        }
-    ], [
-        {
-            "attack": Attack.RESIZING,
-            "params": {
-                "scale": 0.2
-            }
-        }
-    ],
-        [
-            {
-                "attack": Attack.SHARPEN,
-                "params": {
-                    "sigma": 1,
-                    "alpha": 1
-                }
-            }
+    # Best starting value for attacks
+    attacks = [
+        [{"attack": Attack.BLUR, "params": {"sigma": [0.5, 0.5]}}, ],
+        [{"attack": Attack.JPEG, "params": {"QF": 5}}],
+        [{"attack": Attack.AWGN, "params": {"std": 14, "seed": 101, "mean": 0}}],
+        [{"attack": Attack.MEDIAN, "params": {"kernel_size": 3}}],
+        [{"attack": Attack.RESIZING, "params": {"scale": 1}}],
+        [{'attack': Attack.SHARPEN, 'params': {'sigma': 1, 'alpha': 1}}],
+        [{'attack': Attack.SHARPEN, 'params': {'sigma': 0.71, 'alpha': 1}}]
+    ]
 
-        ]]
+    # attacks = prepare_attacks()
+    attacks = prepare_joint_attacks()
 
     watermarked_img_path = 'watermarked_image.bmp'
     watermarked_img = cv2.imread(watermarked_img_path, 0)
     original_img = cv2.imread(ORIGINAL_IMG_PATH, 0)
     watermark = np.load('findbrivateknowledge.npy')
     watermark = cv2.resize(watermark, (32, 32))
+
+    time_start = datetime.datetime.now()
 
     # Execute attacks
     with ProcessPoolExecutor() as executor:
@@ -244,14 +290,33 @@ if __name__ == "__main__":
     for future in futures:
         results.append(future.result())
 
+    # To beat max wpsnr: 51.86934243636088, attack: [{'attack': <Attack.BLUR: 0>, 'params': {'sigma': [0.5, 0.5]}}]
+
     U_wm, S_wm, V_wm = svd(watermark)
     # np.savetxt("U_wm", U_wm)
     # np.savetxt("S_wm", S_wm)
     # np.savetxt("V_wm", V_wm)
 
+    f = []
+
+    max_wpsnr = 0
+    max_a = None
+
     for i, result in enumerate(results):
-        res_attacked, res_contains_w, res_wpsnr = result
-        print(f"Contains w?: {res_contains_w}, WPSNR: {res_wpsnr}")
+        res_attacked, res_contains_w, res_wpsnr, a = result
+
+        if res_contains_w == 0:
+            if max_wpsnr != 9999999:
+                if res_wpsnr > max_wpsnr:
+                    max_wpsnr = res_wpsnr
+                    max_a = a
+        # if res_contains_w == 0:
+        #    print(f"Success w?: {res_contains_w}, WPSNR: {res_wpsnr}")
         # watermarks = extraction(res_attacked, original_img, U_wm, V_wm)
         # watermarks_values = [similarity(watermark, wm) for wm in watermarks]
         # print(watermarks_values, wpsnr(watermarked_img, res_attacked), attacks[i][0]["attack"])
+
+    time_end = datetime.datetime.now()
+
+    print(f"Time taken: {(time_end - time_start).seconds} seconds")
+    print(f"max wpsnr: {max_wpsnr}, attack: {max_a}")
